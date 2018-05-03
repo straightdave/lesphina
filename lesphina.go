d40a76053bc728779dbfe80259fc71da6c7f9ead
Dave Wu <eyaswoo@gmail.com>
2018-05-03 15:17:47 +0800

    update dump and restore

--- lesphina.go
+++ lesphina.go
@@ -1,9 +1,14 @@
 package lesphina
 
 import (
+	"bytes"
+	"compress/gzip"
+	"encoding/base64"
 	"encoding/json"
 	"errors"
+	"io/ioutil"
 	"os"
+	"strings"
 )
 
 var (
@@ -41,14 +46,44 @@ func Read(source string) (*Lesphina, error) {
 	}, nil
 }
 
-func (les *Lesphina) DumpJson() string {
-	return les.Meta.Json()
+func (les *Lesphina) DumpString() string {
+	raw := les.Meta.Json()
+	var buff bytes.Buffer
+	gz := gzip.NewWriter(&buff)
+	if _, err := gz.Write([]byte(raw)); err != nil {
+		panic(err)
+	}
+	if err := gz.Flush(); err != nil {
+		panic(err)
+	}
+	if err := gz.Close(); err != nil {
+		panic(err)
+	}
+	return base64.StdEncoding.EncodeToString(buff.Bytes())
 }
 
-func ReadJson(raw string) (*Lesphina, error) {
-	var meta Meta
-	if err := json.Unmarshal([]byte(raw), &meta); err != nil {
-		return nil, err
+func Restore(raw string) *Lesphina {
+	raw = strings.TrimSpace(raw)
+	zippedStr, err := base64.StdEncoding.DecodeString(raw)
+	if err != nil {
+		panic(err)
 	}
-	return &Lesphina{Meta: &meta}, nil
+
+	rdata := bytes.NewReader(zippedStr)
+	upzippedStr, err := gzip.NewReader(rdata)
+	if err != nil {
+		panic(err)
+	}
+
+	jsonStr, err := ioutil.ReadAll(upzippedStr)
+	if err != nil {
+		panic(err)
+	}
+
+	var meta Meta
+	if err := json.Unmarshal(jsonStr, &meta); err != nil {
+		panic(err)
+	}
+
+	return &Lesphina{Meta: &meta}
 }
---
+++ lesphina_test.go
@@ -0,0 +1,21 @@
+package lesphina
+
+import (
+	"testing"
+)
+
+func TestDumpRestore(t *testing.T) {
+	les, err := Read("test_fixture/myapp.pb.go")
+	if err != nil || les == nil {
+		t.Log(err.Error())
+		t.Fail()
+	}
+
+	// t.Log("meta:" + les.Meta.Json())
+
+	dump := les.DumpString()
+	t.Log("dump:" + dump)
+
+	les1 := Restore(dump)
+	t.Log("restore:" + les1.Meta.Json())
+}
